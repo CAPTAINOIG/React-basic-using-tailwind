8dc9b5c338198066aa3c36351543828f791d119f
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-08-02 08:04:10 +0100

    commit

--- src/component/Filedash/Signin.jsx
+++ src/component/Filedash/Signin.jsx
@@ -39,25 +39,25 @@ const Signin = () => {
     })
   })
   return (
-    <section id='background' className='flex flex-col pb-20 items-center justify-center'>
-      <div className='content bg-white shadow rounded w-[30%] p-5 mt-10'>
+    <section id='background' className='flex flex-col py-20 lg:py-8 md:py-8 pb-20 items-center justify-center'>
+      <div className='content bg-white shadow rounded lg:w-[30%] md:w-[50%] w-[80%] p-5 px-5 mt-10'>
         <div className=''>
           <img className='mx-auto' src={logodark} alt="" />
         </div>
-        <h1 className='my-3 text-center text-black'>Sign In</h1>
-        <form onSubmit={formik.handleSubmit}>
-          <div className='my-7 h-[50px]'>
-            <input className='border mt-1 rounded border-blue-700 py-2 px-4 w-full focus:outline-blue-500' onBlur={formik.handleBlur} onChange={formik.handleChange} type="email" placeholder="email" name='email' />
-            <span className='text-red-500 text-sm'>{formik.touched.email && formik.errors.email}</span>
+        <h1 className='my-5 text-center text-black'>Sign In</h1>
+        <form onSubmit={formik.handleSubmit} className='px-5'>
+          <div className='my-3 h-[50px]'>
+            <input className='border mt-1 h-[35px] rounded border-gray-300 py-2 px-4 w-full focus:outline-blue-700' onBlur={formik.handleBlur} onChange={formik.handleChange} type="email" placeholder="email" name='email' />
+            <span className='text-red-500 text-[12px]'>{formik.touched.email && formik.errors.email}</span>
           </div>
-          <div className='relative my-7 h-[50px]'>
-            <input className='border rounded border-blue-700 py-2 px-4 w-full focus:outline-blue-500' type={isPasswordVisible ? 'text' : 'password'} placeholder="Password" onBlur={formik.handleBlur} onChange={formik.handleChange} name='password' />
+          <div className='relative my-5 h-[50px]'>
+            <input className='border mt-1 h-[35px] rounded border-gray-300 py-2 px-4 w-full focus:outline-blue-700' type={isPasswordVisible ? 'text' : 'password'} placeholder="Password" onBlur={formik.handleBlur} onChange={formik.handleChange} name='password' />
             <span onClick={() => handlePasswordVisible('text')} className='absolute top-[15px] right-5'>{isPasswordVisible ? <span><IoEyeSharp /></span> : <span><IoEyeOffSharp /></span>}</span>
-            <span className='text-red-500 text-sm'>{formik.touched.password && formik.errors.password}</span>
+            <span className='text-red-500 text-[12px]'>{formik.touched.password && formik.errors.password}</span>
           </div>
 
           <div>
-            <div className='flex justify-between my-3'>
+            <div className='flex justify-between my-5'>
               <div className='flex gap-1'>
                 <span><input type="checkbox" /></span>
                 <span>Remember Me</span>
@@ -67,11 +67,11 @@ const Signin = () => {
               </div>
             </div>
           </div>
-          <button type='submit' className='btn btn-primary w-full bg-blue-700 p-2 rounded text-white'>Sign in</button>
+          <button type='submit' className='btn font-bold w-full hover:bg-blue-900 bg-blue-700 p-1 my-3 rounded text-white'>Sign in</button>
         </form>
         <div className='border-b border-gray-300 my-5'></div>
         <p class="text-muted text-center">Login with your social media account.</p>
-        <ul className='flex text-blue-700 gap-7 my-5 px-5'>
+        <ul className='flex text-blue-700 gap-7 my-5 lg:px-5 md:px-5 '>
           <li className='border border-blue-700 rounded-full bg-blue-700 p-1 text-white'>
             <a href="#" class="btn btn-floating btn-facebook">
               <i class="fa fa-facebook"><FaFacebook /></i>
