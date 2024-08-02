422da8285200ac43e5122d9a78eaca7628e183b8
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-08-02 10:31:12 +0100

    commit

--- src/component/Filedash/Signup.jsx
+++ src/component/Filedash/Signup.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import logodark from '../Filedash/image/logodark.png'
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup'
+
+
 
 const Signup = () => {
+
+  let lower = new RegExp(`(?=.*[a-z])`);
+  let upper = new RegExp(`(?=.*[A-Z])`);
+  let number = new RegExp(`(?=.*[0-9])`);
+  let length = new RegExp(`(?=.{8,})`);
+
+  let formik = useFormik({
+    initialValues:{
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+    },
+    onSubmit:(values)=>{
+      console.log(values);
+    },
+    validationSchema:yup.object({
+      firstname: yup.string().required( <span className='flex'><span>This field is required</span></span> ),
+      lastname: yup.string().required( <span className='flex'><span>This field is required</span></span> ),
+      email: yup.string().email('Invalid email format').required( <span className='flex'><span>This field is required</span></span> ),
+      password: yup.string().matches(lower, "Must include lowercase letter").matches(upper, "Must include uppercase letter").matches(number, "Must include a number").matches(length, "Must not be less than 8 characters").required("This field is required"),
+    })
+  })
   return (
     <div id='background' className="form-membership min-h-screen flex items-center justify-center py-10">
       <div className="preloader absolute inset-0 flex items-center justify-center">
@@ -13,21 +40,24 @@ const Signup = () => {
         <img className='mx-auto' src={logodark} alt="" />
         </div>
         <h5 className="text-center mb-6">Create account</h5>
-        <form>
-          <div className="form-group mb-4">
-            <input type="text" className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Firstname" required autoFocus />
+        <form onSubmit={formik.handleSubmit}>
+          <div className="h-[50px] mb-7">
+            <input type="text" className="h-[50px] w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Firstname" name='firstname' onBlur={formik.handleBlur} onChange={formik.handleChange}  autoFocus />
+            <span className='text-red-500 text-[12px]'>{formik.touched.firstname && formik.errors.firstname}</span>
           </div>
-          <div className="form-group mb-4">
-            <input type="text" className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Lastname" required />
+          <div className="h-[50px] mb-7">
+            <input type="text" className="h-[50px] w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Lastname" name='lastname' onBlur={formik.handleBlur} onChange={formik.handleChange}  />
+            <span className='text-red-500 text-[12px]'>{formik.touched.lastname && formik.errors.lastname}</span>
           </div>
-          <div className="form-group mb-4">
-            <input
-              type="email" className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Email" required/>
+          <div className="h-[50px] mb-7">
+            <input type="email" className="h-[50px] w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Email" name='email' onBlur={formik.handleBlur} onChange={formik.handleChange} />
+            <span className='text-red-500 text-[12px]'>{formik.touched.email && formik.errors.email}</span>
           </div>
-          <div className="form-group mb-4">
-            <input type="password" className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Password" required />
+          <div className="h-[50px] mb-7">
+            <input type="password" className="h-[50px] w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600" placeholder="Password" name='password' onBlur={formik.handleBlur} onChange={formik.handleChange} />
+            <span className='text-red-500 text-[12px]'>{formik.touched.password && formik.errors.password}</span>
           </div>
-          <button className="btn btn-primary btn-block w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700"> Register</button>
+          <button type='submit' className="btn btn-primary btn-block w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700"> Register</button>
           <hr className="my-6" />
           <p className="text-center text-gray-600 mb-4">Already have an account?</p>
           <div className='text-center border font-bold text-sm w-[20%] mx-auto border-gray-300  my-5 hover:bg-gray-400 hover:border-gray-300 rounded'>
